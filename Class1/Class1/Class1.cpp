f37a1fc4265f7355c1d4406e0993e9b86fae5d5e
ZhT515 <77432977+ZhT515@users.noreply.github.com>
2021-05-31 11:21:08 +0800

    Add files via upload

--- Class1/Class1/Class1.cpp
+++ Class1/Class1/Class1.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 using namespace std;
 
+/*
+cout 是ostream类的对象
+cin 是istream类的对象
+*/
+
 class Date
 {
+	friend ostream& operator<< (ostream& out,const Date& d);					//友元函数，可以访问私有的
+	friend istream& operator>> (istream& in, Date& d);
 public:
 
 	Date(int year = 1900, int month = 1, int day = 1)			//构造函数
@@ -43,14 +50,29 @@ private:
 	int _day;
 };
 
+ostream& operator<< (ostream & out,const Date&d)				//设为全局，避免抢位置			
+{
+	out << d._year << "-" << d._month << "-" << d._day;
+
+	return out;
+}
+
+istream& operator>> (istream& in, Date& d)						//<<>>是输入输出运算符，所以重载后就用了
+{
+	in>> d._year >> d._month >> d._day;
+	
+	return in;
+}
+
 int main()
 {
 	Date d1;
-	d1.Print(2021, 5, 25);			//处理成了d1.Init(&d1 , 2021, 5, 25);
+	cin >> d1;
 
 	Date d2;
-	d2.Print(2021, 5, 27);
+	cin >> d1;
 
-	Date d3(d1);
-	return 0;
+	cout << d1 <<endl<< d2<<endl;
+
+	
 }
