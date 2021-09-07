c86d2b51f6d8aa31e06d114762e6b79b42367a73
ZhT515 <77432977+ZhT515@users.noreply.github.com>
2021-09-07 20:35:13 +0800

    Add files via upload

---
+++ opencv_test/opencv_test/test.cpp
@@ -0,0 +1,16 @@
+﻿
+#include "stdio.h"
+#include<iostream> 
+#include <opencv2/core.hpp> 
+#include <opencv2/highgui.hpp> 
+using namespace cv;
+int main()
+{
+    Mat img = imread("1.PNG");
+    namedWindow("测试opencv");
+    imshow("测试opencv", img);
+
+    imwrite("save.jpg", img);
+    cvWaitKey(6000);
+}
+
